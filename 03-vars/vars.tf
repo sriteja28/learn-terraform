5c6ce60903b362779ec3b655581c177a99f0b574
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-09-29 07:18:02 +0530

    Playbook

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -17,3 +17,54 @@ output "sample1" {
 output "sample-ext" {
   value = "Value of sample is ${var.sample}"
 }
+
+# variable data types
+# 1. String
+# 2. Number
+# 3. Boolean
+
+# variable types
+# Ansible -- Plain key:value, List, Map/Dict
+# Terraform -- Plain, List, Map/Dict
+
+#Plain
+variable "course" {
+  default = "DevOps training"
+}
+
+#List
+variable "courses" {
+  default = [
+  "DevOps",
+   "AWS",
+    "Python"
+  ]
+}
+
+# Map
+variable "course_info" {
+  default = {
+    DevOps = {
+      name     = "DevOps"
+      timing   = "9:30"
+      duration = 90
+    }
+    AWS = {
+      name     = "AWS"
+      timing   = "10"
+      duration = 10
+    }
+  }
+}
+
+output "course" {
+  value = var.course
+}
+
+output "courses" {
+  value = var.courses
+}
+
+output "course_details" {
+  value = var.course_info
+}
