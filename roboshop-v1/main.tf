6c2495d435925ea0065ca61c8b1fe3bdb8427f69
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-09-29 14:48:38 +0530

    Playbook

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,49 +1,21 @@
-module "frontend" {
+module "instances" {
+  for_each = var.instances
   source = "./ec2"
-  name = "frontend"
+  name = each.key
 }
 
-module "mongodb" {
-  source = "./ec2"
-  name = "mongodb"
-}
+variable "instances" {
+  default = {
 
-module "catalogue" {
-  source = "./ec2"
-  name = "catalogue"
-}
-
-module "redis" {
-  source = "./ec2"
-  name = "redis"
-}
-
-module "user" {
-  source = "./ec2"
-  name = "user"
-}
-
-module "cart" {
-  source = "./ec2"
-  name = "cart"
-}
-
-module "mysql" {
-  source = "./ec2"
-  name = "mysql"
-}
-
-module "shipping" {
-  source = "./ec2"
-  name = "shipping"
-}
-
-module "rabbitmq" {
-  source = "./ec2"
-  name = "rabbitmq"
-}
-
-module "payment" {
-  source = "./ec2"
-  name = "payment"
+    frontend = {}
+    mongodb = {}
+    catalogue = {}
+    redis = {}
+    user = {}
+    cart = {}
+    mysql = {}
+    shipping = {}
+    rabbitmq = {}
+    payment = {}
+  }
 }
