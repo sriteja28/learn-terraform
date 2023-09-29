6a94c2bf8efa07c6f51415583a2de6062e6849d9
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-09-29 08:04:16 +0530

    Playbook

--- 04-datasources/data.tf
+++ 04-datasources/data.tf
@@ -26,7 +26,7 @@ output "sgid" {
 
 
 data "aws_ami" "example" {
-  executable_users = ["amazon"]
+  owners = ["amazon"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   }
