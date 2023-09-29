db55f76f6821b9d891a46e7ae4a4b6d3c876c93f
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-09-29 09:17:16 +0530

    Playbook

--- 08-provisioner/main.tf
+++ 08-provisioner/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "web" {
   provisioner "remote-exec" {
     inline = [
       "sudo labauto ansible",
-      "ansible-pull -i localhost -U https://github.com/sriteja28/roboshop-ansible main.yml -e env=dev -e role_name=frontend -i frontend-dev.sritejadevops.com",
+      "ansible-pull -i localhost -U https://github.com/sriteja28/roboshop-ansible main.yml -e env=dev -e role_name=frontend"
     ]
   }
 }
