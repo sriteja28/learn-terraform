acf910339833835035822edbc09c44cea3c30f70
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-09-29 07:01:40 +0530

    Playbook

---
+++ 02-outputs/outputs.tf
@@ -0,0 +1,12 @@
+output "sample" {
+  value = "Hello World"
+}
+
+# Any string in terraform need to be quoted with Double quotes
+# Single quotes are not supported in terraform
+
+output "sample1" {
+  value = 1000
+}
+
+# Numbers and booleans (true, false)
